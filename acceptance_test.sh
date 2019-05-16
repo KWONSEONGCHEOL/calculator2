43f73ceafc3c799538858413a13c085ba44b9cc0
KWONSEONGCHEOL <zx7613@naver.com>
2019-05-16 13:45:27 +0900

    myip

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
 #test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
-
 CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
 echo $CALCULATOR_PORT
-test $(curl --silent 223.39.140.254 :$CALCULATOR_PORT/sum?a=10\&b=20) -eq 30
+test $(curl --silent 223.39.140.254:$CALCULATOR_PORT/sum?a=10\&b=20) -eq 30
 
 
